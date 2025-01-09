3c229f496e10c0f9f9d21761f3f1745d95483560
luigifg <luigi.gerber@hotmail.com>
2025-01-09 14:10:29 -0300

    git add cors main

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const helmet = require('helmet')
-require('dotenv').config()
-const routes = require('./config/routes')
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const helmet = require('helmet');
+require('dotenv').config();
+const routes = require('./config/routes');
 
-app.use(cookieParser())
-app.use(bodyParser.json())
+app.use(cookieParser());
+app.use(bodyParser.json());
 
 // Adicionando o CORS aqui, antes das rotas
 app.use(cors({
-    origin: process.env.CORS,
+    origin: 'https://feday-app.vercel.app', // Substituindo process.env.CORS pelo domínio direto
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
-app.use(helmet())
+app.use(helmet());
 
-routes.start(app)
+routes.start(app);
 
-module.exports = app
+module.exports = app;
