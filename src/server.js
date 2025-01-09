1eeab35a09f6bf97396585dde4e7a70c41325269
luigifg <luigi.gerber@hotmail.com>
2025-01-09 11:18:48 -0300

    git change db

--- src/server.js
+++ src/server.js
@@ -1,6 +1,21 @@
-const app = require('./app')
-const port = process.env.PORT || 3309
+// const app = require('./app')
+// const port = process.env.PORT || 3309
+
+// app.listen(port, () => {
+//   console.log(`Server UP: Port ${port} - Cors: ${process.env.CORS}`)
+// })
+
+const app = require('./app');
+const cors = require('cors');
+const port = process.env.PORT || 3309;
+
+// Configurando o CORS para permitir apenas o endereço específico
+app.use(
+  cors({
+    origin: 'https://feday-app.vercel.app',
+  })
+);
 
 app.listen(port, () => {
-  console.log(`Server UP: Port ${port} - Cors: ${process.env.CORS}`)
-})
+  console.log(`Server UP: Port ${port} - Cors: https://feday-app.vercel.app`);
+});
