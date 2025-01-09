71756a5180f3f1c8c5d6dc6df8b2fc777b3ed8dc
luigifg <luigi.gerber@hotmail.com>
2025-01-09 13:42:01 -0300

    git env local

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -1,50 +1,50 @@
-// const path = require('path')
-
-// module.exports = {
-//   client: 'mysql',
-//   connection: {
-//     database: process.env.DBNAME,
-//     user: process.env.DBUSER,
-//     password: process.env.DBPASSWORD,
-//     host: process.env.DBHOST,
-    
-//   },
-//   pool: {
-//     min: 2,
-//     max: 10,
-//     acquireTimeoutMillis: 10000
-    
-//   },
-//   migrations: {
-//     tableName: 'knex_migrations',
-//     directory: path.resolve(__dirname,'./migrations'),
-//   },
-//   seeds: {
-//     directory: path.resolve(__dirname,'./seeds'),
-//   },
-// }
-
-// Update with your config settings.
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   client: 'mysql',
   connection: {
-    database: 'railway',
-    user: 'root',
-    password: 'xslOhpaTboJacAVNSjqRPPvfBTSrqTek',
-    host: 'mysql.railway.internal',
+    database: process.env.DBNAME,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    host: process.env.DBHOST,
+    
   },
   pool: {
     min: 2,
     max: 10,
-    acquireTimeoutMillis: 10000,
+    acquireTimeoutMillis: 10000
+    
   },
   migrations: {
     tableName: 'knex_migrations',
-    directory: path.resolve(__dirname, './migrations'),
+    directory: path.resolve(__dirname,'./migrations'),
   },
   seeds: {
-    directory: path.resolve(__dirname, './seeds'),
+    directory: path.resolve(__dirname,'./seeds'),
   },
-};
+}
+
+// Update with your config settings.
+// const path = require('path');
+
+// module.exports = {
+//   client: 'mysql',
+//   connection: {
+//     database: 'railway',
+//     user: 'root',
+//     password: 'xslOhpaTboJacAVNSjqRPPvfBTSrqTek',
+//     host: 'mysql.railway.internal',
+//   },
+//   pool: {
+//     min: 2,
+//     max: 10,
+//     acquireTimeoutMillis: 10000,
+//   },
+//   migrations: {
+//     tableName: 'knex_migrations',
+//     directory: path.resolve(__dirname, './migrations'),
+//   },
+//   seeds: {
+//     directory: path.resolve(__dirname, './seeds'),
+//   },
+// };
